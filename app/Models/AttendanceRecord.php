8f6839e5c1b24f495e4dd67eb340eff8164141d2
Al Fatih Muzakki <malfazakki@gmail.com>
2025-04-22 09:46:50 +0700

    feat(models): add attendance record and session type model
    - Implement AttendanceRecord and SessionType to the models
    - Modify User models to accomodate the relation to the record and session type

---
+++ app/Models/AttendanceRecord.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class AttendanceRecord extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'session_type_id',
+        'date',
+        'status',
+        'notes',
+        'recorder_by',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date' => 'date',
+    ];
+
+    /**
+     * Get the user (santri) associated with this attendance record.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the session type associated with this attendance record.
+     */
+    public function sessionType(): BelongsTo
+    {
+        return $this->belongsTo(SessionType::class);
+    }
+
+    /**
+     * Get the user (mentor) who record this attendace.
+     */
+    public function recorded(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'recorder_by');
+    }
+}
---
+++ app/Models/SessionType.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class SessionType extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'start_time',
+        'end_time',
+    ];
+
+    /**
+     * Get the attendance records for this session type.
+     */
+    public function attendanceRecords(): HasMany
+    {
+        return $this->hasMany(AttendanceRecord::class);
+    }
+}
